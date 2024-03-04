5be5ab9b17e98183b2ba024c474a514cca7e4e7b
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2024-03-04 10:18:45 +0900

    Update js1.js
    dsdftr

--- Abs/js1.js
+++ Abs/js1.js
@@ -210,3 +210,18 @@ switch(laptops) {
   default:
     console.log(`Other companies`)
 }
+//
+let students="graduate"
+switch(students) {
+  case "Undergraduate":
+    console.log(`student of bachelors program`)
+    break;
+  case ("School student"):
+    console.log(`a student who go to school`)
+    break;
+  case ("graduate"):
+    console.log(`a student who finished the bachelors program and continueing education in the next phase`)
+    break;
+  default:
+    console.log(`other student`)
+}
