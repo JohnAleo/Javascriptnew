0a70d999cf04efc2419b2e9f5b0c7dcf53eddb3b
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2024-03-06 21:50:59 +0900

    Update js2.js
    this is a new repository

---
+++ Abs/js2.js
@@ -0,0 +1,14 @@
+let students="graduate"
+switch(students) {
+  case "Undergraduate":
+    console.log(`student of bachelors program`)
+    break;
+  case ("School student"):
+    console.log(`a student who go to school`)
+    break;
+  case ("graduate"):
+    console.log(`a student who finished the bachelors program and continueing education in the next phase`)
+    break;
+  default:
+    console.log(`other student`)
+}
