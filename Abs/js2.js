4321865bb01bbe7d819aa03905e0543606ae0a83
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2024-03-06 22:48:08 +0900

    Update js2.js
    Arrays

--- Abs/js2.js
+++ Abs/js2.js
@@ -1,14 +1,45 @@
-let students="graduate"
-switch(students) {
-  case "Undergraduate":
-    console.log(`student of bachelors program`)
-    break;
-  case ("School student"):
-    console.log(`a student who go to school`)
-    break;
-  case ("graduate"):
-    console.log(`a student who finished the bachelors program and continueing education in the next phase`)
-    break;
-  default:
-    console.log(`other student`)
+// While
+let weather=20;
+
+while (weather >18) {
+  console.log(`havo issiq`);
+  break;
 }
+
+//
+let student=14
+while (student<16) {
+  console.log(`This student have to attend to school lesson`)
+  break;
+}
+//
+let price=1000;
+while (price>900) {
+  console.log(`This will be the newest iphone`)
+  break;
+};
+
+//do while
+let grade=95;
+do{
+  console.log(`This gives a scholarship`)
+  break;
+} while (grade>86);
+//
+let temperature=36.6;
+do {
+  console.log(`this is a norm temperature`)
+  break;
+}while(temperature>=36.6 && temperature<36.8)
+//
+
+let speed=120;
+do{
+  console.log(`This can cause a charge`);
+  break;
+} while(speed>100)
+
+
+//Arrays
+let hobbylist=["Book","movie","training","hiking"];
+console.log(hobbylist.length);
